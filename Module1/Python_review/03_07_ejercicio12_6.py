c5ba232fe2961a6d45d1081a3fbb2d8f56ffd874
VelasquezE <esvelasquezs@unal.edu.co>
2025-09-08 08:43:28 -0500

    Finish exercise 12.6

--- Module1/Python_review/03_07_ejercicio12_6.py
+++ Module1/Python_review/03_07_ejercicio12_6.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # Given
 time_points = [0, 1, 2, 3, 4]  # seconds
 particles = {"P1", "P2", "P3"}  # unique IDs
@@ -10,11 +12,16 @@ trajectories = {
 # Task: Find which particles came within 0.2 units of (0,0)
 
 epsilon = 0.2
-wanted_point = 0
+wanted_point = 0.0
+selected_particles = {"P1": False, "P2": False, "P3": False}
 
-print(f"Las partículas que pasaron 0.2 unidades de (0, 0) son: \n")
+print(f"Las partículas que pasaron 0.2 unidades de (0, 0) son:")
 
 for id in particles:
     for coordinate in trajectories[id]:
-        if (coordinate[0] - wanted_point) <= epsilon and (coordinate[1] - wanted_point) <= epsilon:
-            print(f"{id}: {coordinate}\n")
+        if np.abs(coordinate[0] - wanted_point) <= epsilon and np.abs(coordinate[1] - wanted_point) <= epsilon:
+            selected_particles[id] = True
+
+for particle in selected_particles:
+    if selected_particles[particle] == True:
+        print(f"{particle} ")
